9bb13baebd67f3289fb9282c14c7d3b0c251d0cb
Phillip Marques <phillipmarq@hotmail.com>
2024-08-09 18:05:29 -0300

    Field filling tests

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,9 +1,27 @@
 /// <reference types="Cypress" />
 
 describe('Central de Atendimento ao Cliente TAT', function() {
-    it('verifica o título da aplicação', function() {
-        cy.visit('./src/index.html')
+
+    beforeEach(() => cy.visit('./src/index.html'))
+
+    it('Teste de verificação de titulo', function() {
 
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
     })
+
+    it('Preenchimento de campos obrigatórios e envio de formulário', () => {
+
+        cy.get('input[id="firstName"]').type('Phillip')
+        .should('have.value','Phillip')
+        cy.get('input[id="lastName"]').type('Marques')
+        .should('have.value','Marques')
+        cy.get('input[id="email"]').type('phmarq@marq.com')
+        .should('have.value','phmarq@marq.com')
+        cy.get('textarea[id="open-text-area"]').type('Teste de preenchimento de campos obrigatórios e envio de formulário', {delay: 0})
+        .should('have.value','Teste de preenchimento de campos obrigatórios e envio de formulário')
+        cy.get('button[type="submit"]').click()
+        cy.get('span[class="success"]').should('be.visible')
+    })
+
+
   })
