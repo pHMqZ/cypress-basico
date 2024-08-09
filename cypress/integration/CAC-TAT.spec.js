32cfa7f74e4b9d6012b657ba15ea82687e33a300
Phillip Marques <phillipmarq@hotmail.com>
2024-08-09 20:48:46 -0300

    Email validation test

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -10,18 +10,23 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     })
 
     it('Preenchimento de campos obrigatórios e envio de formulário', () => {
-
         cy.get('input[id="firstName"]').type('Phillip')
-        .should('have.value','Phillip')
         cy.get('input[id="lastName"]').type('Marques')
-        .should('have.value','Marques')
         cy.get('input[id="email"]').type('phmarq@marq.com')
-        .should('have.value','phmarq@marq.com')
         cy.get('textarea[id="open-text-area"]').type('Teste de preenchimento de campos obrigatórios e envio de formulário', {delay: 0})
-        .should('have.value','Teste de preenchimento de campos obrigatórios e envio de formulário')
         cy.get('button[type="submit"]').click()
-        cy.get('span[class="success"]').should('be.visible')
+        
+        cy.get('.success').should('be.visible')
     })
 
+    it('Validação de máscara de e-mail', function(){
+        cy.get('input[id="firstName"]').type('Phillip')
+        cy.get('input[id="lastName"]').type('Marques')
+        cy.get('input[id="email"]').type('phmarqmarq.com')
+        cy.get('textarea[id="open-text-area"]').type('Teste de preenchimento de campos obrigatórios e envio de formulário', {delay: 0})
+        cy.get('button[type="submit"]').click()
+
+        cy.get('.error').should('be.visible')
+    })
 
   })
