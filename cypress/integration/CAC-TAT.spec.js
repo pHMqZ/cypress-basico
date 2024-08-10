12b51dc8ca0ae99ce4a8745eb0c40d70f837dacc
Phillip Marques <phillipmarq@hotmail.com>
2024-08-09 21:08:49 -0300

    Phone field validation test

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -10,23 +10,37 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     })
 
     it('Preenchimento de campos obrigatórios e envio de formulário', () => {
-        cy.get('input[id="firstName"]').type('Phillip')
-        cy.get('input[id="lastName"]').type('Marques')
-        cy.get('input[id="email"]').type('phmarq@marq.com')
-        cy.get('textarea[id="open-text-area"]').type('Teste de preenchimento de campos obrigatórios e envio de formulário', {delay: 0})
+        cy.get('#firstName').type('Phillip')
+        cy.get('#lastName').type('Marques')
+        cy.get('#email').type('phmarq@marq.com')
+        cy.get('#open-text-area').type('Teste de preenchimento de campos obrigatórios e envio de formulário', {delay: 0})
         cy.get('button[type="submit"]').click()
         
         cy.get('.success').should('be.visible')
     })
 
     it('Validação de máscara de e-mail', function(){
-        cy.get('input[id="firstName"]').type('Phillip')
-        cy.get('input[id="lastName"]').type('Marques')
-        cy.get('input[id="email"]').type('phmarqmarq.com')
-        cy.get('textarea[id="open-text-area"]').type('Teste de preenchimento de campos obrigatórios e envio de formulário', {delay: 0})
+        cy.get('#firstName').type('Phillip')
+        cy.get('#lastName').type('Marques')
+        cy.get('#email').type('phmarqmarq.com')
+        cy.get('#open-text-area').type('Teste')
         cy.get('button[type="submit"]').click()
 
         cy.get('.error').should('be.visible')
     })
 
+    it('Validação de preenchimento com valores não numericos no campo telefone', ()=>{
+        cy.get('#phone')
+            .type('abcdef').should('have.value', '')
+    })
+
+    it('Validação de obrigatoriedade de telefone', () =>{
+        cy.get('#firstName').type('Phillip')
+        cy.get('#lastName').type('Marques')
+        cy.get('#email').type('phmarq@marq.com')
+        cy.get('#phone-checkbox').click()
+        cy.get('#open-text-area').type('Teste')
+        
+        cy.get('button[type="submit"]').click()
+    })
   })
